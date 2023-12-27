15d46f9085bede47ea6102a50e721ee9cbc6966d
fodorati2000 <32171508+fodorati2000@users.noreply.github.com>
2023-12-27 10:53:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import wallpaper from './img/illustration-article.svg';
+import avatar from './img/image-avatar.webp';
 import './App.css';
+import fontBold from './static/Figtree-ExtraBold.ttf';
+import fontSemi from './static/Figtree-SemiBold.ttf';
+import fontVariable from './Figtree-VariableFont_wght.ttf';
+import { useState } from 'react';
+
+
+function Card({ active,id, type, date, title, description, avatar, author,onElementClick})
+{
+
+  let classActive = "cardInactive";
+  if(active===Number(id)){
+    classActive ="cardActive";
+  }else{
+    classActive ="cardInactive";
+  }
+  return(
+  <div className={classActive} onClick={onElementClick}>
+    <div className="header">
+      <img className="wallpaper" src={wallpaper} alt="wallpaper" />
+    </div>
+    <div className="body">
+      <div className="type" style={{ fontFamily: { fontBold } }}>{type}</div>
+      <div className="date" style={{ fontFamily: { fontSemi } }}>Published: {date}</div>
+      <div className="title" style={{ fontFamily: { fontBold } }}>{title}</div>
+      <div className="description" style={{ fontFamily: { fontVariable } }}>{description}</div>
+      <div className="author" style={{ fontFamily: { fontBold } }}>
+        <img className="avatar" src={avatar} alt="avatar" />
+        {author}
+      </div>
+    </div>
+  </div>
+  );
+}
 
 function App() {
+  const [active, setActive] = useState(null);
+
+  function handleClick(i)
+  {
+    setActive(i);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" >
+      <header className="main" >
+      <Card active={active} id="0" onElementClick={()=>handleClick(0)} type="Learning" date="21 Dec 2023" title="HTML & CSS foundations" description="These languages are the backbone of every website, defining structure, content, and presentation. " avatar={avatar} author="Greg Hooper"/>
       </header>
     </div>
   );
